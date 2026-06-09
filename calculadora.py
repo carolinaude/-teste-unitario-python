02cd6a534be160d656ae2bf65b2648bcdb926940
carolinaude <carolina.ude@estudante.ufla.br>
2026-06-08 21:21:21 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,24 @@
+def somar(a,b):
+    """Retorna a soma de dois números."""
+    return a + b
+def subtrair (a,b):
+    """Retorna a subtração de dois números"""
+    return a - b
+def multiplicar (a,b):
+    """Retorna a multiplicação de dois números"""
+    return a * b
+def dividir(a, b):
+    """
+    Retorna a divisão de dois números.
+
+    Se o segundo número for zero, o Python irá gerar um erro.
+    """
+    return a / b
+
+def potencia(a,b):
+        """Retorna a potência de um número."""
+  return a ** b
+
+
+
+
